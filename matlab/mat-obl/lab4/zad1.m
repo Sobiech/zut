e3c0b3b1a60762bb4066f9ebfc310cf56440bd76
Sobiech <kacper.sobisz@gmail.com>
2017-12-03 10:06:35 +0100

    zad1 MAT-OBL

--- matlab/mat-obl/lab4/zad1.m
+++ matlab/mat-obl/lab4/zad1.m
@@ -5,7 +5,7 @@ close all
 format long
 
 MATRIX_SIZES = [ 5, 10, 100, 500, 1000, 3000, 5000 ];
-ACCURACY =  [ 0.1, 0.01, 0.001 ]; 
+ACCURACY     =  [ 0.1, 0.01, 0.001 ]; 
 
 for  k = 1 : length(ACCURACY)
     
@@ -15,19 +15,49 @@ for  k = 1 : length(ACCURACY)
         
         matrixSize = MATRIX_SIZES(i);
         A = [];
+        b = [];
+        
         for  x = 1 : matrixSize 
             for y = 1 : matrixSize
                 A(x,y) = rand();
-            end   
+            end
+            b(x,:) = rand();
         end
          
         A = A + ( eye(matrixSize) * 10 );
-        X = ones(matrixSize);
+        WZ = [ A b ];
+        xn = 0;
+        for x = 1 : matrixSize
+            
+            for y = 1 : matrixSize
+                if x == y 
+                    WZ(x, : ) = WZ(x,:)./WZ(x,x);
+                    WZ(x, 1 : matrixSize) = WZ(x, 1:matrixSize) * ( -1 );
+                    WZ( x , x ) = 0;
+                end
+               
+            end
+             
+        end
         
+        tempX = 0;
         
-        %B = A * X;
-
+        x = 1;
+        
+        vectorSize = length(WZ);
+        
+        while true
+            
+            d = max ( abs ( WZ (:, x) - tempX ) );
+            val  = WZ(:, vectorSize - 1).* tempX .* WZ(:, vectorSize);
+            tempX = WZ(:, vectorSize);
+            
+            if d <= eps
+                break
+            end
+        end
     end
+    
     toc
 end
 
