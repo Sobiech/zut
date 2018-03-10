e2ae7b9879e617c42f87719caa994da3982f3903
Sobiech <kacper.sobisz@gmail.com>
2018-03-10 16:39:51 +0100

    Dodanie ab

--- matlab/adieb/lab1/lab1.m
+++ matlab/adieb/lab1/lab1.m
@@ -16,12 +16,12 @@ A = [ 1 2 3 4 5 6 ];
 B = [ 11 30 14 10 33 22];
 
 hiSq = 0;
-for i = 1 : size(B)
+for i = 1 : length(B)
     suma = (( B(i) - 20) ^ 2 )/ 20 ;
     hiSq = hiSq + suma;
 end
 
-p = 1 - chi2cdf(hiSq, 6 - 1);
+p = 1 - chi2cdf(hiSq, 5);
 fprintf('p = %f\n', p );
 if p <= alfa
     disp('Rozk³ad jest nie oczekiwany: p <= alfa');
@@ -36,20 +36,44 @@ alfa = input('Wprowadz alfa: ');
 % 2 - okragle i zielone
 % 3 - pomarszone i zolte
 % 4 - okragle i zolte
-A = [ 1, 3, 3, 9];
-B = [ 32, 108, 101, 315 ];
+A = [ 1 3 3 9];
+B = [ 32 108 101 315 ];
 
-sumB = sum(B);
 sumA = sum(A);
+sumB = sum(B);
 
 hiSq2 = 0;
-for i = 1 : size(B)
-    np_i = ( sumB / sumA ) * B(i);
-    suma = (( B(i) - np_i) ^ 2) / np_i;
-    hiSq2 = hiSq2 + suma;
+for i = 1 : length(B)
+    np_i = ( sumB / sumA ) * A(i);
+    suma2 = (( B(i) - np_i) ^ 2) / np_i;
+    hiSq2 = hiSq2 + suma2;
 end
 
-p = 1 - chi2cdf(hiSq2, 4 - 1);
+p = 1 - chi2cdf(hiSq2, 3);
+fprintf('p = %f\n', p);
+if p <= alfa
+    disp('Rozk³ad jest nie oczekiwany: p <= alfa');
+else
+    disp('Rozk³ad jest oczekiwany: p > alfa');
+end
+
+%% start
+fprintf('\nZadanie 3 \n' );
+alfa = input('Wprowadz alfa: ');
+A = [ 97 103 109 115 121 127 133 139 ];
+B = [ 3 7 11 20 28 19 10 2 ];
+
+sumA = sum(A);
+sumB = sum(B);
+
+hiSq2 = 0;
+for i = 1 : length(B)
+    np_i = ( sumB / sumA ) * A(i);
+    suma2 = (( B(i) - np_i) ^ 2) / np_i;
+    hiSq2 = hiSq2 + suma2;
+end
+
+p = 1 - chi2cdf(hiSq2, 3);
 fprintf('p = %f\n', p);
 if p <= alfa
     disp('Rozk³ad jest nie oczekiwany: p <= alfa');
