8e76d230c14c737e1ee02ff6fc62f124b5e80efa
Sobiech <kacper.sobisz@gmail.com>
2018-10-07 15:21:23 +0200

    Rozmyte

--- matlab/adieb/lab8/lab8.m
+++ matlab/adieb/lab8/lab8.m
@@ -35,55 +35,77 @@ for key = 1 : k
     C.put(key,xCov);
 end
 
-
-
-% mieszanka rozkladow normalnych
-p = [,];
-for i = 1:n
+deltaLL = 0;
+%while ( deltaLL <= delta )
     
-    for j = 1:k
+    %deltaLL
         
-        x_i = X(i,:);
-        m_j = M(j,:);
-        c_j = C.get(j);
-        
-        %odleglosc mahalanobisa
-        d = ( x_i - m_j ) * inv(c_j) * ( x_i - m_j )';
-        
-        Cj_detSqrt = sqrt(det( c_j ));
-        exp_d = exp( (-1/2) * d );
-        
-        p(i,j) =  exp_d / ( 2 * pi * Cj_detSqrt );
-    end
-end
+    % mieszanka rozkladow normalnych
+    p = [,];
+    for i = 1:n
 
-% krok E
-g = [,];
-for i = 1 : n
-   
-    for j = 1 : k        
-        licznik = W(j) * p(i,j);
-        mianownik = sum(p(i,:) * W(j));
-        g(i,j) = licznik / mianownik;
+        for j = 1:k
+
+            x_i = X(i,:);
+            m_j = M(j,:);
+            c_j = C.get(j);
+
+            %odleglosc mahalanobisa
+            d = ( x_i - m_j ) * inv(c_j) * ( x_i - m_j )';
+
+            Cj_detSqrt = sqrt(det( c_j ));
+            exp_d = exp( (-1/2) * d );
+
+            p(i,j) =  exp_d / ( 2 * pi * Cj_detSqrt );
+        end
     end
+
+    % krok E
+    g = [,];
+    for i = 1 : n
+
+        for j = 1 : k        
+            licznik = W(j) * p(i,j);
+            mianownik = sum(p(i,:) * W(j));
+            g(i,j) = licznik / mianownik;
+        end
+
+    end    
+
+    % funkcja wiarygodnosci
+    func_ll_sum = sum(log(p*W));
+
+    % maksymalizacja logarytmu funkcji
+    w = sum(g)/n;
+    M = [];
+
+    for j = 1 : k
+        suma = 0;
+        for i = 1 : n 
+            suma = suma + ( g(i,j) * sum(X(i,:)));
+        end
+        M(j) = suma;
+    end
+
+
+    for j = 1 : k
+
+        calc_cj = 0;
+        m_j = M(j);
+        for i = 1 : n 
+            x_i = X(i,:);
+            calc_cj =  ( x_i - m_j )' * g(i,j) * ( x_i - m_j );
+            
+        end
+        calc_cj = ( X
+        
+        C_j  = calc_cj / ( n * w(j));
+        C.put(j, C_j);
+    end
+
+    % porównywanie wartoœci funkcji wiarygodnoœci z wartoœci¹ funkcji z
+    % poprzedniej iteracji 
     
-end    
-
-func_ll_sum = sum(log(p*W))
-% funkcja wiarygodnosci
-%func_ll_sum = 0;
-
-% to samo  co ln. 72
-%{ 
-for i = 1 : n
-    class_sum = 0;
-    for j = 1 : k   
-        class_sum = sum(p(i,:) * W(j));
-    end
+    deltaLL =  func_ll_sum - deltaLL;
     
-    func_ll_sum = func_ll_sum + log(class_sum);
-end  
-%}
-
-
-
+%end    
